098f9517132aba27a1791585f549cf27a524f6a2
Garve Hays <gkhays@gmail.com>
2025-03-24 11:13:48 -0500

    Observe <platform>/src convention

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for language in java python; do
-    docker build -t ipv6tester-$language -f "Dockerfile.$language" .
+    docker build -t ipv6tester-$language -f "docker/src/Dockerfile.$language" .
 done
